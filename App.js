c0bf38823263a44a9bcf95b4d1103c465d58f7be
ShreyashiMahanta <53649054+ShreyashiMahanta@users.noreply.github.com>
2020-05-22 13:20:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUpLogInScreen from './screens/SignupLoginScreen';
+import ExchangeScreen from './screens/exchangeScreen';
+import HomeScreen from './screens/homeScreen';
+import {createAppContainer,switchNavigator} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation/bottom-tabs';
+import AppTabNavigator from './components/AppTabNavigator';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SignUpLogInScreen/>
+      <AppTabNavigator/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const TabNavigator = createBottomTabNavigator({
+  HomeScreen: HomeScreen,
+  ExchangeScreen: ExchangeScreen,
+})
+const AppContainer = createAppContainer({switchNavigator})
